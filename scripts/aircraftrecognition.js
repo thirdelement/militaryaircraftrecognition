fbf0370e5545269840beee18b7116a28a1afc89d
thirdelement <gamechanger@Hotmail.co.uk>
2021-02-01 21:23:32 +0000

    Add CSS file; Add FontAwesome CDN and icons for correct and incorrect answer; update JS to toggle icons on and off

--- scripts/aircraftrecognition.js
+++ scripts/aircraftrecognition.js
@@ -4,3 +4,31 @@ function correctAlert() {
     function incorrectAlert() {
         alert("Incorrect!"); 
     }
+
+function correctAnswer() {
+  var check = document.getElementById("correct");
+  if (check.style.display === "none") {
+      check.style.display = "inline";
+    //check.style.display = check.style.display == "none" ? "inline" : "none";
+  } else {
+    check.style.display = "none";
+  }
+}
+
+function incorrectAnswer() {
+  var check = document.getElementsByClassName("incorrect");
+  if (check.style.display === "none") {
+    check.style.display = "inline-block";
+  } else {
+    check.style.display = "none";
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
