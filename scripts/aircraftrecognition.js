a271f8ff1b941257db7ea9eb3975fab0dac9bb1e
thirdelement <gamechanger@Hotmail.co.uk>
2021-02-08 19:00:47 +0000

    Update JS file to select random elements from array and use these to populate HTML elements for quiz image and button names

--- scripts/aircraftrecognition.js
+++ scripts/aircraftrecognition.js
@@ -1,4 +1,4 @@
-$(".correct-answer").click(function () {
+$("correct-answer").click(function () {
     $(".correct").show();
 });
 
@@ -32,13 +32,13 @@ function displayAircraft() {
     return f35
 }
 
-function displayAircraft1() {
+//function displayAircraft1() {
     // $(f35.name).show();
     //console.log(f35)
-    document.getElementById("demo").innerHTML = f35.image
-    document.getElementById("demo").innerHTML = f22.image
-    document.getElementById("demo").innerHTML = fa18.image
-}
+    //document.getElementById("demo").innerHTML = f35.image
+    //document.getElementById("demo").innerHTML = f22.image
+    //document.getElementById("demo").innerHTML = fa18.image
+//}
 
 //$(".correct-answer").click(function(){
 //console.log(f35)
@@ -49,16 +49,48 @@ function randomAircraft() {
     ["F-22 Raptor", "<img src='assets/images/f22.png'  alt='F22 Raptor' />"], 
     ["F/A-18 Hornet", "<img src='assets/images/fa18.png'  alt='F/A-18 Hornet' />"], 
     ["Chengdu J-10", "<img src='assets/images/j10.png'  alt='Chengdu J-10' />"]]
-    aircraftToUse1 = aircraft[Math.floor(Math.random() * aircraft.length)];
-    aircraftToUse2 = aircraft[Math.floor(Math.random() * (aircraft.length - aircraftToUse1))];
 
-    // do something with the selected value
-    document.getElementById("demo").innerHTML = aircraftToUse1[1];
-    document.getElementById("incorrect-answer1").innerHTML = aircraftToUse1[0];
+    //Randomly select aircraft from array
+    let aircraftToUse1 = Math.floor(Math.random() * aircraft.length);
+    let aircraftChosen1 = aircraft[aircraftToUse1];
+
+    //Remove selected aircraft from array
+    aircraft.splice(aircraftToUse1, 1); 
+
+    //Randomly select another aircraft from array
+    let aircraftToUse2 = Math.floor(Math.random() * aircraft.length);
+    let aircraftChosen2 = aircraft[aircraftToUse2];
+
+    //Remove another aircraft from array
+    aircraft.splice(aircraftToUse2, 1); 
+
+    //Randomly select another aircraft from array
+    let aircraftToUse3 = Math.floor(Math.random() * aircraft.length);
+    let aircraftChosen3 = aircraft[aircraftToUse3];
+
+    //Remove a third aircraft from array
+    aircraft.splice(aircraftToUse3, 1); 
+
+    //Randomly select another aircraft from array
+    let aircraftToUse4 = Math.floor(Math.random() * aircraft.length);
+    let aircraftChosen4 = aircraft[aircraftToUse4];
+
+    //Remove a third aircraft from array
+    aircraft.splice(aircraftToUse4, 1); 
+
+    // Add aircraft image to paragraph element
+    document.getElementById("demo").innerHTML = aircraftChosen1[1];
+
+    // Add aircraft name to button
+    document.getElementById("correct-answer").innerHTML = aircraftChosen1[0];
     
-    //while(aircraftToUse2 !== aircraftToUse1){
-    document.getElementById("incorrect-answer2").innerHTML = aircraftToUse2[0];
+    // Add aircraft name to button
+    document.getElementById("incorrect-answer1").innerHTML = aircraftChosen2[0];
+    
+    // Add aircraft name to button
+    document.getElementById("incorrect-answer2").innerHTML = aircraftChosen3[0];
+    
+     // Add aircraft name to button
+    document.getElementById("incorrect-answer3").innerHTML = aircraftChosen4[0];
     };
-
-
-// Code from https://stackoverflow.com/questions/14422233/how-do-you-select-a-random-variable-within-a-function-in-javascript
+// Code for randomly selecting from array from: https://stackoverflow.com/questions/57059564/how-to-exclude-an-array-element-from-a-random-pick
